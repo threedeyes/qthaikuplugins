6cc3b06440c1fea62beb5be42b60303d81b8b4c1
Gerasim Troeglazov <3dEyes@gmail.com>
2026-08-11 11:54:34 +1000

    icons: update build script

--- icons/build.sh
+++ icons/build.sh
@@ -11,33 +11,41 @@ cp -f --remove-destination index.theme $dataDir/icons/haiku
 
 echo "Install actions icons"
 cp -f --remove-destination actions/*.svg $dataDir/icons/haiku/actions/22
-rm -rf $dataDir/icons/haiku/actions/{12,16,32,48,64}
+rm -rf $dataDir/icons/haiku/actions/{12,16,24,32,48,64}
 ln -rfs $dataDir/icons/haiku/actions/22 $dataDir/icons/haiku/actions/12
 ln -rfs $dataDir/icons/haiku/actions/22 $dataDir/icons/haiku/actions/16
+ln -rfs $dataDir/icons/haiku/actions/22 $dataDir/icons/haiku/actions/24
 ln -rfs $dataDir/icons/haiku/actions/22 $dataDir/icons/haiku/actions/32
 ln -rfs $dataDir/icons/haiku/actions/22 $dataDir/icons/haiku/actions/48
 ln -rfs $dataDir/icons/haiku/actions/22 $dataDir/icons/haiku/actions/64
 
 echo "Install categories icons"
 cp -f --remove-destination categories/*.svg $dataDir/icons/haiku/categories/32
+rm -rf $dataDir/icons/haiku/categories/{22,24}
+ln -rfs $dataDir/icons/haiku/categories/22 $dataDir/icons/haiku/categories/22
+ln -rfs $dataDir/icons/haiku/categories/22 $dataDir/icons/haiku/categories/24
 
 echo "Install devices icons"
 cp -f --remove-destination devices/*.svg $dataDir/icons/haiku/devices/22
-rm -rf $dataDir/icons/haiku/devices/{16,64,symbolic}
+rm -rf $dataDir/icons/haiku/devices/{16,24,32,64,symbolic}
 ln -rfs $dataDir/icons/haiku/devices/22 $dataDir/icons/haiku/devices/16
+ln -rfs $dataDir/icons/haiku/devices/22 $dataDir/icons/haiku/devices/24
+ln -rfs $dataDir/icons/haiku/devices/22 $dataDir/icons/haiku/devices/32
 ln -rfs $dataDir/icons/haiku/devices/22 $dataDir/icons/haiku/devices/64
 
 echo "Install mimetypes icons"
 cp -f --remove-destination mimetypes/*.svg $dataDir/icons/haiku/mimetypes/22
-rm -rf $dataDir/icons/haiku/mimetypes/{16,32,64}
+rm -rf $dataDir/icons/haiku/mimetypes/{16,24,32,64}
 ln -rfs $dataDir/icons/haiku/mimetypes/22 $dataDir/icons/haiku/mimetypes/16
+ln -rfs $dataDir/icons/haiku/mimetypes/22 $dataDir/icons/haiku/mimetypes/24
 ln -rfs $dataDir/icons/haiku/mimetypes/22 $dataDir/icons/haiku/mimetypes/32
 ln -rfs $dataDir/icons/haiku/mimetypes/22 $dataDir/icons/haiku/mimetypes/64
 
 echo "Install places icons"
 cp -f --remove-destination places/*.svg $dataDir/icons/haiku/places/22
-rm -rf $dataDir/icons/haiku/places/{16,32,48,64,96,symbolic}
+rm -rf $dataDir/icons/haiku/places/{16,24,32,48,64,96,symbolic}
 ln -rfs $dataDir/icons/haiku/places/22 $dataDir/icons/haiku/places/16
+ln -rfs $dataDir/icons/haiku/places/22 $dataDir/icons/haiku/places/24
 ln -rfs $dataDir/icons/haiku/places/22 $dataDir/icons/haiku/places/32
 ln -rfs $dataDir/icons/haiku/places/22 $dataDir/icons/haiku/places/48
 ln -rfs $dataDir/icons/haiku/places/22 $dataDir/icons/haiku/places/64
@@ -46,8 +54,8 @@ ln -rfs $dataDir/icons/haiku/places/22 $dataDir/icons/haiku/places/96
 echo "Install status icons"
 cp -f --remove-destination status/*.svg $dataDir/icons/haiku/status/16
 cp -f --remove-destination status/*.svg $dataDir/icons/haiku/status/22
+cp -f --remove-destination status/*.svg $dataDir/icons/haiku/status/24
 cp -f --remove-destination status/*.svg $dataDir/icons/haiku/status/32
-cp -f --remove-destination status/*.svg $dataDir/icons/haiku/status/48
 cp -f --remove-destination status/*.svg $dataDir/icons/haiku/status/64
 rm -rf $dataDir/icons/haiku/status/symbolic
 
@@ -55,16 +63,22 @@ echo "Install emblems icons"
 cp -f --remove-destination emblems/*.svg $dataDir/icons/haiku/emblems/8
 cp -f --remove-destination emblems/*.svg $dataDir/icons/haiku/emblems/16
 cp -f --remove-destination emblems/*.svg $dataDir/icons/haiku/emblems/22
+cp -f --remove-destination emblems/*.svg $dataDir/icons/haiku/emblems/24
 
 echo "Install preferences icons"
 cp -f --remove-destination preferences/*.svg $dataDir/icons/haiku/preferences/32
+rm -rf $dataDir/icons/haiku/preferences/{22,24}
+ln -rfs $dataDir/icons/haiku/preferences/22 $dataDir/icons/haiku/preferences/24
+ln -rfs $dataDir/icons/haiku/preferences/22 $dataDir/icons/haiku/preferences/24
 
 echo "Install apps icons"
 cp -f --remove-destination apps/*.svg $dataDir/icons/haiku/apps/16
 cp -f --remove-destination apps/*.svg $dataDir/icons/haiku/apps/22
+cp -f --remove-destination apps/*.svg $dataDir/icons/haiku/apps/24
 cp -f --remove-destination apps/*.svg $dataDir/icons/haiku/apps/32
 cp -f --remove-destination apps/*.svg $dataDir/icons/haiku/apps/48
 cp -f --remove-destination apps/*.svg $dataDir/icons/haiku/apps/64
 
 echo "Install emotes icons"
 cp -f --remove-destination emotes/*.svg $dataDir/icons/haiku/emotes/22
+cp -f --remove-destination emotes/*.svg $dataDir/icons/haiku/emotes/24
